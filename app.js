19374636cbf1df4bb437051fd9731d6d4eac480c
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-06-23 16:25:20 -0700

    set up animals and sounds

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-// import functions
 
-// reference needed DOM elements
-
-
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+const catSound = document.getElementById('cat-sounds');
+const catPic = document.getElementById('cat-pic');
+  
+catPic.addEventListener('click', () => {
+    catSound.play();
+});
+const dogSound = document.getElementById('dog-sounds');
+const dogPic = document.getElementById('dog-pic');
+  
+dogPic.addEventListener('click', () => {
+    dogSound.play();
+});
+const horseSound = document.getElementById('horse-sounds');
+const horsePic = document.getElementById('horse-pic');
+  
+horsePic.addEventListener('click', () => {
+    horseSound.play();
+});
